14dd0baa22a74d32ef3a04739bfc02707110d349
wulechuan <wulechuan@live.com>
2014-09-24 19:56:19 +0800

    我的商户类页面大部分已经完成；发布信息类页面未做。

--- wwwroot/scripts/this-app/lianlian-app.js
+++ wwwroot/scripts/this-app/lianlian-app.js
@@ -1,6 +1,15 @@
 (function () {
-	return false;
 	var _fakeHeader = document.querySelector('body > header');
+
+	_fakeHeader.addEventListener('click', function() {
+		window.location.assign('my-booth--index.html');
+	});
+	
+	return false;
 	document.body.removeChild(_fakeHeader);
 	delete _fakeHeader;
 })();
+
+var appEnv = {
+	isIOS: true
+};
