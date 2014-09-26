81aa5a0e600368d2beab360142d37aebb5683eee
wulechuan <wulechuan@live.com>
2014-09-26 18:27:14 +0800

    仅差几个页面了。

--- wwwroot/scripts/this-app/lianlian-app.js
+++ wwwroot/scripts/this-app/lianlian-app.js
@@ -17,9 +17,58 @@ var appEnv = {
 };
 
 
+function setMaxHeight(element, subtractSelectors) {
+	subtractSelectors = subtractSelectors || [];
+	var _subtractHeights = [];
+	var _subtractHeightTotal = 0;
+	for (var _s = 0; _s < subtractSelectors.length; _s++) {
+		var _el = subtractSelectors[_s];
+		if (!(_el instanceof HTMLElement)) {
+			_el = document.querySelector( _el );
+		}
+		var _elHeight = !!_el ? parseFloat(_el.offsetHeight) : 0;
+		// var _elHeight = !!_el ? parseFloat(window.getComputedStyle(_el).height) : 0;
+		_subtractHeights[_s] = _elHeight;
+		_subtractHeightTotal += _elHeight;
+	};
+
+	var _maxHeight = Math.max(0, (window.innerHeight - _subtractHeightTotal));
+	// console.log(window.innerHeight + ' - [', _subtractHeights.join(', '), '] = ' + _maxHeight);
+
+	if (_maxHeight > 0) element.style.maxHeight = _maxHeight + 'px';
+};
 
 
-function CreateGraphPieController(rootElement, initOptions) {
+function tryShowButtonSectionTooltip (toShowTooltip) {
+	var _buttonSection = document.querySelector('section.button');
+
+	_buttonSection.style.paddingTop = toShowTooltip ? '0px' : '';
+	_buttonSection.querySelector('.tooltip').style.display = toShowTooltip ? '' : 'none';
+};
+
+
+function updateAdStatus(element, status) {
+	// status:
+		// 0: 暂无提交
+		// 1: 等待审核
+		// 2: 审核通过
+		// 3: 审核失败
+
+	var _attr = false;
+
+	switch (status) {
+		case 0: _attr = 'zan-wu-ti-jiao';		break;
+		case 1: _attr = 'deng-dai-shen-he';		break;
+		case 2: _attr = 'shen-he-tong-guo';		break;
+		case 3: _attr = 'shen-he-shi-bai';		break;
+		default: break;
+	}
+
+	if (_attr) element.setAttribute('ad-status', _attr);
+};
+
+
+function CreateLLyGraphPieController(rootElement, initOptions) {
 	if (!rootElement instanceof HTMLElement || rootElement.id.length<1) return undefined;
 
 	var _thisPieGraph = {
@@ -62,12 +111,12 @@ function CreateGraphPieController(rootElement, initOptions) {
 
 		options = options || {};
 
-		if (options.hasOwnProperty('dataElement') && options.dataElement instanceof HTMLElement) {
-			this.dataElement = options.dataElement;
-		}
-
 		if (options.hasOwnProperty('onupdate') && typeof options.onupdate === 'function') {
 			this.onupdate = options.onupdate;
+		}
+
+		if (options.hasOwnProperty('dataElement') && options.dataElement instanceof HTMLElement) {
+			this.dataElement = options.dataElement;
 		}
 
 
@@ -77,10 +126,6 @@ function CreateGraphPieController(rootElement, initOptions) {
 			} else {
 				this.updateByAngles(options.startAngle, options.endAngle);
 			}
-		}
-
-		if (options.hasOwnProperty('onupdate') && typeof options.onupdate === 'function') {
-			this.onupdate = options.onupdate;
 		}
 
 		return _ok;
@@ -167,8 +212,7 @@ function CreateGraphPieController(rootElement, initOptions) {
 		this.rootElement.style.visibility = 'hidden';
 		this.rootElement.style.visibility = '';
 	}
-} // Factory:CreateGraphPieController
-
+}; // Factory:CreateLLyGraphPieController
 
 
 function CreateLLYSwitcherController(rootElement, initOptions) {
@@ -224,12 +268,12 @@ function CreateLLYSwitcherController(rootElement, initOptions) {
 
 	function _config(options) {
 		var _ok = true;
+		options = options || {};
 
 		if (options.hasOwnProperty('onswitch') && typeof options.onswitch === 'function') {
 			this.onswitch = options.onswitch;
 		}
 
-		options = options || {};
 		if (options.hasOwnProperty('switchTo')) this.switchTo(options.switchTo);
 
 		return _ok;
@@ -264,4 +308,108 @@ function CreateLLYSwitcherController(rootElement, initOptions) {
 		this.rootElement.style.visibility = 'hidden';
 		this.rootElement.style.visibility = '';
 	}
-} // Factory:CreateLLYSwitcherController
+}; // Factory:CreateLLYSwitcherController
+
+
+function CreateLLYPrettyCheckboxController(rootElement, initOptions) {
+	if (!rootElement instanceof HTMLElement) return undefined;
+
+	var _thisCheckbox = {
+		rootElement: rootElement,
+		input: undefined,
+
+		check: function (checked, event) { return _check.call(this, checked, event); },
+		onchange: undefined,
+		oncheck: undefined,
+		onuncheck: undefined,
+	}
+
+	return (function () {
+		var _ok;
+
+		_ok = _init.call(this);
+		if (!_ok) return undefined;
+
+		_ok = _config.call(this, initOptions);
+		if (!_ok) return undefined;
+
+		return this;
+	}).call(_thisCheckbox);
+
+	function _init () {
+		var _ok = true;
+
+		Object.defineProperty(this, 'checked', {
+			get: function () { return this.input.checked; },
+			set: function (checked) { return this.check(checked, null); }
+		});
+
+		this.rootElement.setAttribute('lly-pretty-checkbox', ''); // just for sure
+		this.input = this.rootElement.querySelector('input[type="checkbox"]');
+
+		if (!(this.input instanceof HTMLElement || this.input.tagName.toLowerCase() != 'input')) {
+			_ok = false;
+			return _ok;
+		}
+
+		this.rootElement.addEventListener('click', function (event) { _thisCheckbox.check(undefined, event); });
+		this.input.addEventListener('click', function (event) { _thisCheckbox.check(undefined, event); });
+
+		return _ok;
+	}
+
+	function _config(options) {
+		var _ok = true;
+		options = options || {};
+
+		if (options.hasOwnProperty('onchange') && typeof options.onchange === 'function') {
+			this.onchange = options.onchange;
+		}
+
+		if (options.hasOwnProperty('oncheck') && typeof options.oncheck === 'function') {
+			this.oncheck = options.oncheck;
+		}
+
+		if (options.hasOwnProperty('onuncheck') && typeof options.onuncheck === 'function') {
+			this.onuncheck = options.onuncheck;
+		}
+
+		if (options.hasOwnProperty('checked')) {
+			this.check(options.checked, null);
+		} else {
+			this.check(this.input.checked, null);
+		}
+		return _ok;
+	}
+
+	function _check(checked, event) {
+		// console.log('raw value:', checked, event.target);
+		if (typeof checked==='undefined') {
+			if (event && event.target && event.target === this.input ) {
+				// auto toggle by browser
+			} else {
+				// console.log('not the input element');
+				this.input.checked = !this.input.checked;
+			}
+		} else {
+			checked = !!checked
+			this.input.checked = checked;
+		}
+
+		if (this.input.checked) {
+			this.rootElement.setAttribute('checked', '');
+		} else {
+			this.rootElement.removeAttribute('checked');
+		}
+
+		if (typeof _thisCheckbox.onchange === 'function') _thisCheckbox.onchange(event);
+		if (typeof _thisCheckbox.oncheck === 'function' && this.input.checked) _thisCheckbox.oncheck(event);
+		if (typeof _thisCheckbox.onuncheck === 'function' && !this.input.checked) _thisCheckbox.onuncheck(event);
+
+		// force IE to redraw this element
+		this.rootElement.style.visibility = 'hidden';
+		this.rootElement.style.visibility = '';
+
+		return this.input.checked;
+	}
+}; // Factory:CreateLLYPrettyCheckboxController
